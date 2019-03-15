ddf24278b8307a3a866d0ba9c8b9ef997715a459
Amine SAFI <amine.safi@esprit.tn>
2019-03-16 00:33:59 +0100

    New Todo component to show the detail and update it later

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -18,6 +18,10 @@ export class TodoDataService {
     return this.http.delete(`http://localhost:8080/users/${username}/todos/${id}`);
   }
 
+  retrieveTodo(username,id){
+    return this.http.get<Todo>(`http://localhost:8080/users/${username}/todos/${id}`);
+  }
+
 
 
 }
